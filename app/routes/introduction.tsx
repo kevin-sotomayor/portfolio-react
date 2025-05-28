22398ab4e63fc2909778d7a28af2bec742e11dd1
La-li-lu-le-lo <kevin.sotomayor@outlook.fr>
2025-05-28 10:58:14 +0200

    both versions of the introduction page available

--- app/routes/introduction.tsx
+++ app/routes/introduction.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/introduction";
 import { useState, } from "react";
 
-import { languageCookie, } from "../utils/cookies";
+import { languageCookieUtils, } from "../utils/cookies";
 import content_fr from "../data/introduction_fr.json";
 import content_en from "../data/introduction_en.json";
 
@@ -14,7 +14,7 @@ interface DataInterface {
 
 export async function loader({ request }: Route.LoaderArgs) {
 	const rawCookie = await request.headers.get("Cookie");
-	const cookie = await languageCookie.parse(rawCookie);
+	const cookie = await languageCookieUtils.parse(rawCookie);
 	return cookie;
 } 
 
@@ -22,7 +22,15 @@ export default function HomePage({ loaderData }: Route.ComponentProps) {
 	const cookie = loaderData;
 	return (
 		<>
-			
+			{cookie && cookie.language === "fr" ? (
+				content_fr.introduction.map((paragraph, index) => (
+					<p key={index}>{paragraph}</p>
+				))
+			) : (
+				content_en.introduction.map((paragraph, index) => (
+					<p key={index}>{paragraph}</p>
+				))
+			)}
 		</>
 	);
 }
