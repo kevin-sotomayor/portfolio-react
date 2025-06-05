298009e537a4396e9c2825b67ee9bce6a32f6de1
La-li-lu-le-lo <kevin.sotomayor@outlook.fr>
2025-06-05 16:32:52 +0200

    did the contact page

--- app/routes/contact.tsx
+++ app/routes/contact.tsx
@@ -1,6 +1,7 @@
 import type { Route } from "./+types/contact";
 
 import pp from "../assets/pp.png";
+import "../styles/contact.css";
 
 
 
@@ -16,7 +17,16 @@ export default function ContactPage() {
 	return (
 		<main className="app-contact">
 			<div className="app-contact__content">
-				<img src={pp} alt="ASCII ART Kevin Sotomayor" />
+				<div className="app-contact__content__chat">
+					<p>Let's get in touch !</p>
+					<img src={pp} alt="ASCII ART Kevin Sotomayor" />
+				</div>
+				<ul className="app-contact__content__socials">
+					<li><a href="https://www.linkedin.com/in/kevin-sotomayor-316b7a271/">Linkedin</a></li>
+					<li><a href="mailto:kevin.sotomayor@outlook.fr">Mail</a></li>
+					<li><a href="https://www.malt.fr/profile/kevinsotomayor1">Malt</a></li>
+					<li><a href="https://www.discord.com/users/255352308777353226">Discord</a></li>
+				</ul>
 			</div>
 		</main>
 	)
