273c730c67285607fd0136eb5e81edf8e434706e
La-li-lu-le-lo <kevin.sotomayor@outlook.fr>
2025-06-13 10:11:58 +0200

    added interface for language prop

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,11 +1,12 @@
 import type { Route, } from "./+types/Layout";
 import { Form, useLocation, } from "react-router";
-import type { LanguageInterface } from "../utils/types";
+import type { LanguagePropInterface } from "../utils/types";
 
 
 
-export default function FooterComponent({ languageProp }: LanguageInterface) {
+export default function FooterComponent({ languageProp }: LanguagePropInterface) {
 	const currentLocation = useLocation();
+	console.log(languageProp);
 	return (
 		<footer className="app-footer">
 			{languageProp === "fr" ? (
